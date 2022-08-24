b75b4fc7cf5fca3c50040203da478a43bea63548
Luca <lubar105@hhu.de>
2022-08-25 01:58:55 +0200

    Freestanding Rust Binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Dont link the Rust std library
+#![no_main] // disable rust entry points
+
+use core::panic::PanicInfo;
+
+#[panic_handler] // this function is called on panic
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // dont mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // since the linker looks for a function called _start(), this is our new entry point
+    loop {}
 }
