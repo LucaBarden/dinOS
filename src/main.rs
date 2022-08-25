42519fc15173b4227d40c46803ad282e0edc2dc7
Luca <lubar105@hhu.de>
2022-08-25 17:22:47 +0200

    added custom testing framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std] // Dont link the Rust std library
 #![no_main] // disable rust entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 mod vga_buffer;
 
@@ -16,5 +20,18 @@ pub extern "C" fn _start() -> ! {
     // since the linker looks for a function called _start(), this is our new entry point
 
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
