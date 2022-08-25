0c8378053299015a2e9fb4da768609faeee025c0
Luca <lubar105@hhu.de>
2022-08-25 20:09:05 +0200

    fixed race condition in output test

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -163,10 +163,16 @@ fn test_println_many() {
 
 #[test_case]
 fn test_println_output() {
-    let s = "Something that fits into one line";
-    println!("{}", s);
-    for (i, c) in s.chars().enumerate() {
-        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-        assert_eq!(char::from(screen_char.ascii_character), c);
-    }
+    use core::fmt::Write;
+    use x86_64::instructions::interrupts;
+
+    let s = "Some test string that fits on a single line";
+    interrupts::without_interrupts(|| {
+        let mut writer = WRITER.lock();
+        writeln!(writer, "\n{}", s).expect("writeln failed");
+        for (i, c) in s.chars().enumerate() {
+            let screen_char = writer.buffer.chars[BUFFER_HEIGHT - 2][i].read();
+            assert_eq!(char::from(screen_char.ascii_character), c);
+        }
+    });
 }
