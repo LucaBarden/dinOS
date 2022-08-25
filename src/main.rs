01fc1af6b818801a662b46ca409b86f750db5b9f
Luca <lubar105@hhu.de>
2022-08-25 19:21:25 +0200

    task state segment added

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     din_os::init();
 
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
