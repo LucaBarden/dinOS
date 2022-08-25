6c615fa37477a0aa1e0836abdaaf689b0c3e5d2a
Luca <lubar105@hhu.de>
2022-08-25 02:29:34 +0200

    first print to screen via bytes / added bootloader

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
+
 #[no_mangle] // dont mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // since the linker looks for a function called _start(), this is our new entry point
+    
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
