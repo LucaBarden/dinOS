27d8c9d3b99eefe8c9dfe55732bfa4973de19031
Luca <lubar105@hhu.de>
2022-08-25 17:58:51 +0200

    added Testable trait and updated test_runner

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 mod serial;
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -37,11 +37,26 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -50,7 +65,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(1, 2);
     serial_println!("[ok]");
 }
 
