19094b94583f361e29dc45c2794656c9b77b2c60
Luca <lubar105@hhu.de>
2022-08-25 19:14:45 +0200

    handler for double faults

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     din_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
